4991a653ed2699479413ee455b203ef6412ae065
Denver Schouw <dschouw@gmail.com>
2022-11-08 20:41:35 +0200

    First commit, supported routes

---
+++ server.js
@@ -0,0 +1,17 @@
+//use expressjs and create a default route returning a string
+const express = require('express');
+const app = express();
+const port = env.port || 3000;
+
+const supportedRoutes = [
+    {"/pharmcies": "retruns an array of pharmacies in a specific area"}
+}
+app.get('/', (req, res) => {
+    res.send(supportedRoutes);
+    }
+);
+//start the server
+app.listen(port, () => {
+    console.log('Server started on port 3000');
+    });
+
