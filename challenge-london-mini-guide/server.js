99d367853918c365786502df0272e1c7e4790d50
Denver Schouw <dschouw@gmail.com>
2022-11-09 19:15:46 +0200

    fix(data) : Fix sample data
    The sample data used in the first draft fix

--- challenge-london-mini-guide/server.js
+++ challenge-london-mini-guide/server.js
@@ -1,17 +1,18 @@
 //use expressjs and create a default route returning a string
 const express = require('express');
 const app = express();
-const port = env.port || 3000;
+const port = process.env.port || 3000;
 
 const supportedRoutes = [
     {"/pharmcies": "retruns an array of pharmacies in a specific area"}
-}
+]
+
 app.get('/', (req, res) => {
     res.send(supportedRoutes);
     }
 );
 //start the server
 app.listen(port, () => {
-    console.log('Server started on port 3000');
+    console.log(`Server started on port ${port}`);
     });
 
